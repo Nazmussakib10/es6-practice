aae8df0fff7c13d9205dc57a1cf1363b84fb17a4
Nazmussakib10 <sakib.cse133@gmail.com>
2020-03-03 01:49:13 +0600

    threedots js practice

---
+++ threedots.js
@@ -0,0 +1,11 @@
+const ages=[12,23,14,15];
+const ages2=[23,34,12];
+const ages3=[34,33,35,56];
+const allAges=[...ages,...ages2,6,...ages3];
+
+const business=450;
+const minister=500;
+const sochib=600;
+const takaPoisha=[400,500,700];
+const maximum=Math.max(...takaPoisha);
+console.log(maximum);
